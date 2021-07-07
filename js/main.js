16e590ccf66d37a8205f8d3b31b4a890b15afa00
nahidmahmud538 <nahid.mahmud538@gmail.com>
2021-07-07 22:38:41 +0600

    Team Section and Testimonial Section Done

---
+++ js/main.js
@@ -0,0 +1,36 @@
+//Feature Items Carousel
+$('.feature-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+//Testimonial Carousel
+$('.testimonial-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
